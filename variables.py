9fb222367f90759bbc01407f214a308060eeb80f
ValeryBel13 <Cerkov94@gmail.com>
2024-12-05 19:13:38 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+completed_homeworks=12
+hours_spent=1.5
+course_name="Python"
+time_per_assigment=hours_spent / completed_homeworks
+print("Курс",course_name,",всего задач:",completed_homeworks,",затрачено часов:",hours_spent,",среднее время выполнения",time_per_assigment,"часа.")
